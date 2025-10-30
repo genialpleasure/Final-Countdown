df60a454076550dd03514c840e37d3869bd93d18
genialpleasure <isaacnpleasure01@gmail.com>
2025-10-31 00:13:16 +0100

    added a portal to the dialog that pops up

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,5 +1,5 @@
 import { useImperativeHandle, useRef } from "react"
-
+import { createPortal } from "react-dom";
 export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
     const dialog = useRef();
 
@@ -15,7 +15,7 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
         };
     });
 
-    return <dialog ref={dialog} className="result-modal">
+    return createPortal(<dialog ref={dialog} className="result-modal">
         {userLost && <h2>You Lost</h2>}
         {!userLost && <h2>Your Score : {score}</h2>}
         <p>You target target time was <strong>{targetTime} seconds.</strong></p>
@@ -23,5 +23,7 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
         <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
+    );
 }
